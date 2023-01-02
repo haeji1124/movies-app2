95c6d5e3b660864e39371d00affde36da69e45db
haeji1124 <46364329+haeji1124@users.noreply.github.com>
2023-01-03 01:11:42 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
+function App() {
+  return (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/movie/:id" element={<Detail/>}></Route>
+      <Route path="/" element={<Home/>}>
+      </Route>
+    </Routes>
+  </BrowserRouter>
+  )
+}
+
+export default App;
